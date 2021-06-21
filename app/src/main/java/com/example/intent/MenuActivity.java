a9debaf5adcf7ea1803867fa8a2eb684f8db6de9
Administrator <taegwan3857@gmail.com>
2021-06-21 14:12:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/intent/MenuActivity.java
@@ -0,0 +1,31 @@
+package com.example.intent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("name", "mike"); // (key, value)
+
+                setResult(Activity.RESULT_OK, intent); // 정상 응답
+                finish();
+            }
+        });
+    }
+}
